9f481fa7452de16454b851b1bdb87a5c0d626458
Aleksander Korostelin <admin@flexits.info>
2022-01-05 20:17:02 +0500

    final

--- SimplePaint/FormMain.cs
+++ SimplePaint/FormMain.cs
@@ -1,15 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 
 namespace SimplePaint
 {
